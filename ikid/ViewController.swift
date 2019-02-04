5ca0b0a9e8658ebcd2fbed5f5b562ed19605bfcc
Student User <studentuser@Students-MBP.hsd1.wa.comcast.net>
2019-02-03 16:58:19 -0800

    Starting files.

---
+++ ikid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ikid
+//
+//  Created by Student User on 2/3/19.
+//  Copyright © 2019 University of Washington. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
